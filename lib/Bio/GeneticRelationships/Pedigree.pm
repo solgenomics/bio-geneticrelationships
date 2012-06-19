2c3e9a7ebdb2891bcae5098fe35db7fa957cb698
Jeremy D. Edwards <jde22@cornell.edu>
2012-06-19 12:57:09 -0400

    add pedigree drawing function

--- lib/Bio/GeneticRelationships/Pedigree.pm
+++ lib/Bio/GeneticRelationships/Pedigree.pm
@@ -9,7 +9,7 @@ use Bio::GeneticRelationships::Individual;
 
 =head1 NAME
 
-    Pedigree - Representation of a pedigree
+    Bio::GeneticRelationships::Pedigree - Pedigree of an individual
 
 =head1 SYNOPSIS
 
@@ -17,7 +17,7 @@ use Bio::GeneticRelationships::Individual;
 
 =head1 DESCRIPTION
 
-    This class stores pedigree information.
+    This class stores an individual's pedigree.
 
 =head2 Methods
 
@@ -42,6 +42,169 @@ has 'name' => (isa => 'Str',is => 'rw', predicate => 'has_name');
 has 'female_parent' => (isa =>'Bio::GeneticRelationships::Individual', is => 'rw', predicate => 'has_female_parent');
 has 'male_parent' => (isa =>'Bio::GeneticRelationships::Individual', is => 'rw', predicate => 'has_male_parent');
 has 'cross_type' => (isa =>'CrossType', is => 'rw', predicate => 'has_cross_type');
+
+
+sub get_pedigree_string {
+    my $self = shift;
+    my $pedigree_str = '';
+    $pedigree_str = $pedigree_str."(";
+    if ($self->has_female_parent()){
+	if ($self->get_female_parent()->has_pedigree()){
+	    $pedigree_str = $pedigree_str.$self->get_female_parent()->get_pedigree_string();
+	}
+	else {
+	    $pedigree_str = $pedigree_str.$self->get_female_parent()->get_name();
+	}
+    }
+    else {
+	$pedigree_str = $pedigree_str."?";
+    }
+
+
+    $pedigree_str = $pedigree_str." X ";
+
+    if ($self->has_male_parent()){
+	if ($self->get_male_parent()->has_pedigree()){
+	    $pedigree_str = $pedigree_str.$self->get_male_parent()->get_pedigree_string();
+	}
+	else {
+	    $pedigree_str = $pedigree_str.$self->get_male_parent()->get_name();
+	}
+    }
+    else {
+	$pedigree_str = $pedigree_str."?";
+    }
+    $pedigree_str = $pedigree_str.")";
+}
+
+
+
+sub traverse_pedigree {
+    #my $self = shift;
+    my $trav_ped = shift; #female pedigree
+    my $current_node_id = shift; #female ID
+    my $current_node_name = shift;
+    my %nodes;
+    my %joins;
+    my $female_parent_id = "female_parent_of_$current_node_id";
+    my $male_parent_id = "male_parent_of_$current_node_id";
+    my $female_parent_name;
+    my $male_parent_name;
+   
+    if ($trav_ped->has_female_parent()){
+	$joins{$female_parent_id} = $current_node_id;
+	if ($trav_ped->get_female_parent()->has_name()){
+	    $female_parent_name =  $trav_ped->get_female_parent()->get_name();
+	    
+	}
+	else {
+	    $female_parent_name = '';
+	}
+	if ($trav_ped->get_female_parent()->has_pedigree()) {
+	    my (%returned_nodes,%returned_joins) = traverse_pedigree($trav_ped->get_female_parent()->get_pedigree(),$female_parent_id,$female_parent_name);
+	    @nodes{keys %returned_nodes} = values %returned_nodes;
+	    @joins{keys %returned_joins} = values %returned_joins;
+	}
+    }
+    else {
+	 $female_parent_name = '?';
+    }
+
+    if ($trav_ped->has_male_parent()){
+	$joins{$male_parent_id} = $current_node_id;
+	if ($trav_ped->get_male_parent()->has_name()){
+	    $male_parent_name =  $trav_ped->get_male_parent()->get_name();
+	    
+	}
+	else {
+	    $male_parent_name = '';
+	}
+	if ($trav_ped->get_male_parent()->has_pedigree()) {
+	    my (%returned_nodes,%returned_joins) = traverse_pedigree($trav_ped->get_male_parent()->get_pedigree(),$male_parent_id,$male_parent_name);
+	    @nodes{keys %returned_nodes} = values %returned_nodes;
+	    @joins{keys %returned_joins} = values %returned_joins;
+	}
+    }
+    else {
+	 $male_parent_name = '?';
+    }
+
+    
+    
+    $nodes{$female_parent_id} = $female_parent_name;
+    $nodes{$male_parent_id} = $male_parent_name;
+
+    return (\%nodes,\%joins);
+}
+
+sub draw_graphviz {
+    my $self = shift;
+    my $current_node_id = shift;
+    my $current_node_name = shift;
+    my %nodes;
+    my %joins;
+    my $graphviz_text;
+    #write graphviz header - append lines to $graphviz_text
+    my $female_parent_id = "female_parent_of_$current_node_id";
+    my $male_parent_id = "male_parent_of_$current_node_id";
+    my $female_parent_name;
+    my $male_parent_name;
+
+    $nodes{$current_node_id} = $current_node_name;
+    
+    if ($self->has_female_parent()) {
+	$joins{$female_parent_id} = $current_node_id;    
+        
+	if ($self->get_female_parent()->has_name()){
+	    $female_parent_name = $self->get_female_parent()->get_name();
+	}
+	else {
+	    $female_parent_name = '';
+	}
+	$nodes{$female_parent_id} = $female_parent_name;
+	if ($self->get_female_parent()->has_pedigree()){
+	    my ($returned_nodes,$returned_joins) = traverse_pedigree($self->get_female_parent()->get_pedigree(),$female_parent_id,$female_parent_name);
+	    @nodes{keys %$returned_nodes} = values %$returned_nodes;
+	    @joins{keys %$returned_joins} = values %$returned_joins;
+	}
+    }
+
+
+    if ($self->has_male_parent()) {
+	$joins{$male_parent_id} = $current_node_id;    
+        
+	if ($self->get_male_parent()->has_name()){
+	    $male_parent_name = $self->get_male_parent()->get_name();
+	}
+	else {
+	    $male_parent_name = '';
+	}
+	$nodes{$male_parent_id} = $male_parent_name;
+	if ($self->get_male_parent()->has_pedigree()){
+	    my ($returned_nodes,$returned_joins) = traverse_pedigree($self->get_male_parent()->get_pedigree(),$male_parent_id,$male_parent_name);
+	    @nodes{keys %$returned_nodes} = values %$returned_nodes;
+	    @joins{keys %$returned_joins} = values %$returned_joins;
+	}
+    }
+
+
+    
+    #write nodes to graphviz
+    foreach my $node_key (keys %nodes) {
+	#print STDERR "Node: $node_key $nodes{$node_key} \n";
+    }
+     #write joins to graphviz
+    foreach my $join_key (keys %joins){
+	#print STDERR "Join: $join_key $joins{$join_key} \n";
+
+   }
+
+    #write graphviz footer if needed
+
+   1;
+}
+
+
 
 ###
 1;#do not remove
--- t/Bio/GeneticRelationships/pedigree.t
+++ t/Bio/GeneticRelationships/pedigree.t
@@ -27,10 +27,11 @@ use warnings;
 use autodie;
 
 
-use Test::More tests => 17;
+use Test::More tests => 26;
 BEGIN {use_ok( 'Bio::GeneticRelationships::Pedigree' ); }
 require_ok( 'Bio::GeneticRelationships::Pedigree' );
 ok (my $pedigree = Bio::GeneticRelationships::Pedigree->new());
+ok (my $pedigree_2 = Bio::GeneticRelationships::Pedigree->new());
 
 #test name method
 ok ($pedigree->set_name('test_name'));
@@ -39,16 +40,27 @@ is ($pedigree->get_name(),'test_name');
 #test methods for adding parents
 ok (my $female_individual = Bio::GeneticRelationships::Individual->new());
 ok (my $male_individual = Bio::GeneticRelationships::Individual->new());
-ok ($female_individual->set_name('female_test_name'));
-ok ($male_individual->set_name('male_test_name'));
+ok ($female_individual->set_name('female_A'));
+ok ($male_individual->set_name('male_B'));
+ok (my $female_individual_2 = Bio::GeneticRelationships::Individual->new());
+ok (my $male_individual_2 = Bio::GeneticRelationships::Individual->new());
+ok ($female_individual_2->set_name('female_C'));
+ok ($male_individual_2->set_name('male_D'));
+
+ok ($female_individual->set_pedigree($pedigree_2));
+
 ok ($pedigree->set_female_parent($female_individual));
 ok ($pedigree->set_male_parent($male_individual));
 is ($pedigree->get_female_parent(),$female_individual);
 is ($pedigree->get_male_parent(),$male_individual);
-is ($pedigree->get_female_parent()->get_name(),'female_test_name');
-is ($pedigree->get_male_parent()->get_name(),'male_test_name');
+is ($pedigree->get_female_parent()->get_name(),'female_A');
+is ($pedigree->get_male_parent()->get_name(),'male_B');
+ok ($pedigree_2->set_female_parent($female_individual_2));
+ok ($pedigree_2->set_male_parent($male_individual_2));
+
 
 #test cross type method
 ok ($pedigree->set_cross_type('single_cross'));
 is ($pedigree->get_cross_type(),'single_cross');
-
+#ok (print STDERR $pedigree->get_pedigree_string()."\n");
+ok ($pedigree->draw_graphviz("root","root"));
