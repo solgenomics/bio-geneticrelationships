8221a66cb92b3fd594382bfcb7776cf6d2636a5e
Jeremy D. Edwards <jde22@cornell.edu>
2012-06-21 12:12:11 -0400

    add pedigree string as purdy format function and test tree with selfing

--- lib/Bio/GeneticRelationships/Pedigree.pm
+++ lib/Bio/GeneticRelationships/Pedigree.pm
@@ -42,6 +42,146 @@ has 'name' => (isa => 'Str',is => 'rw', predicate => 'has_name');
 has 'female_parent' => (isa =>'Bio::GeneticRelationships::Individual', is => 'rw', predicate => 'has_female_parent');
 has 'male_parent' => (isa =>'Bio::GeneticRelationships::Individual', is => 'rw', predicate => 'has_male_parent');
 has 'cross_type' => (isa =>'CrossType', is => 'rw', predicate => 'has_cross_type');
+has 'selection_name' => (isa => 'Str',is => 'rw', predicate => 'has_selection_name');
+
+
+sub get_pedigree_string_purdy {
+    my $self = shift;
+    my ($depth,$purdy,$bcount) = $self->get_pedigree_string_purdy_with_depth();
+    return $purdy;
+}
+
+sub get_pedigree_string_purdy_with_depth {
+    my $self = shift;
+    my $previous = shift;
+    my $female_pedigree;
+    my $male_pedigree;
+    my $current_pedigree;
+    my $female_pedigree_depth = 1;
+    my $male_pedigree_depth = 1;
+    my $deepest_depth;
+    my $cross_indicator;
+    my $is_selfed;
+    my $is_backcross;
+    my $backcross_count = 0;
+    my $previous_was_selfed = 0;
+    my $previous_was_backcrossed = 0;
+    my $recurrent_parent;
+    my $recurrent_parent_isa;
+
+    if (defined($previous)){
+	if ($previous->get_cross_type() eq 'self') {
+    	   $previous_was_selfed  = 1;
+	}
+	if ($previous->get_cross_type() eq 'backcross') {
+	    $previous_was_backcrossed = 1;
+	    if ($self->get_cross_type() eq 'backcross') {
+		if ($self->get_female_parent()->get_name() eq $previous->get_female_parent->get_name()){
+		    $recurrent_parent = $self->get_female_parent->get_name();
+		    $recurrent_parent_isa = 'female';
+		}
+		elsif ($self->get_male_parent()->get_name() eq $previous->get_male_parent->get_name()){
+		    $recurrent_parent = $self->get_male_parent->get_name();
+		    $recurrent_parent_isa = 'male';
+		}
+		else {
+		    #
+		}
+	    }
+	}
+    }
+
+
+
+    if ($self->get_cross_type() eq "self" ){
+	$is_selfed = 1;
+    }
+    if ($self->get_cross_type() eq "backcross"){
+	$is_backcross = 1;
+	$backcross_count = 1;
+    }
+
+
+    if ($self->has_female_parent()){
+	if ($self->get_female_parent()->has_pedigree()){
+	    my ($depth, $returned_pedigree, $b_count) = $self->get_female_parent()->get_pedigree()->get_pedigree_string_purdy_with_depth($self);
+	    $female_pedigree = $returned_pedigree;
+	    $female_pedigree_depth += $depth;
+	    $backcross_count += $b_count; 
+	    if ($self->get_female_parent()->has_name()) {
+		$female_pedigree = $female_pedigree.'('.$self->get_female_parent()->get_name().')';
+	    }
+	}
+	else {
+	    $female_pedigree = $self->get_female_parent()->get_name();
+	}
+    }
+    else {
+	$female_pedigree = "?";
+    }
+
+
+    if ($self->has_male_parent()){
+	if ($self->get_male_parent()->has_pedigree()){
+	    my ($depth, $returned_pedigree) = $self->get_male_parent()->get_pedigree()->get_pedigree_string_purdy_with_depth($self);
+	    $male_pedigree = $returned_pedigree;
+	    $male_pedigree_depth += $depth;
+	    if ($self->get_male_parent()->has_name()) {
+		$male_pedigree = $male_pedigree.'('.$self->get_male_parent()->get_name().')';
+	    }
+	}
+	else {
+	    $male_pedigree = $self->get_male_parent()->get_name();
+	}
+    }
+    else {
+	$male_pedigree = "?";
+    }
+    
+
+    if ($female_pedigree_depth > $male_pedigree_depth) {
+	$deepest_depth = $female_pedigree_depth;
+    }
+    else {
+	$deepest_depth = $male_pedigree_depth;
+    }
+    if ($deepest_depth == 1) {
+	$cross_indicator = '/';
+    }
+    elsif ($deepest_depth == 2) {
+	$cross_indicator = '//';
+    }
+    else {
+	$cross_indicator = '/'.$deepest_depth.'/';
+    }
+
+
+    if ($self->get_cross_type() eq "self" ){
+	my $selection_name;
+	
+	if ($self->has_selection_name()){
+	    $selection_name = $self->get_selection_name();
+	}
+	else {
+	    $selection_name = '?';
+	}
+	if ($previous_was_selfed == 1){
+	    $current_pedigree = $female_pedigree.'-'.$selection_name;
+	}
+	else {
+	    $current_pedigree = '['.$female_pedigree.']-'.$selection_name;
+	}
+	$deepest_depth -= 1;
+    }
+    elsif ($self->get_cross_type() eq "backcross") {
+	#deal with backcrosses
+    }
+    else {
+	$current_pedigree = $female_pedigree.$cross_indicator.$male_pedigree;
+    }
+
+    return ($deepest_depth,$current_pedigree,$backcross_count);
+}
 
 
 sub get_pedigree_string {
@@ -50,7 +190,7 @@ sub get_pedigree_string {
     $pedigree_str = $pedigree_str."(";
     if ($self->has_female_parent()){
 	if ($self->get_female_parent()->has_pedigree()){
-	    $pedigree_str = $pedigree_str.$self->get_female_parent()->get_pedigree_string();
+	    $pedigree_str = $pedigree_str.$self->get_female_parent()->get_pedigree()->get_pedigree_string();
 	}
 	else {
 	    $pedigree_str = $pedigree_str.$self->get_female_parent()->get_name();
@@ -65,7 +205,7 @@ sub get_pedigree_string {
 
     if ($self->has_male_parent()){
 	if ($self->get_male_parent()->has_pedigree()){
-	    $pedigree_str = $pedigree_str.$self->get_male_parent()->get_pedigree_string();
+	    $pedigree_str = $pedigree_str.$self->get_male_parent()->get_pedigree()->get_pedigree_string();
 	}
 	else {
 	    $pedigree_str = $pedigree_str.$self->get_male_parent()->get_name();
